6ae7d296c1f46ede9799c1ce3b72a1aca44476dc
@s.roertgen <sroertgen@gmail.com>
2023-04-04 09:36:26 +0200

    add addedbyEvent property

--- src/etl/parseEvent.js
+++ src/etl/parseEvent.js
@@ -1,34 +1,46 @@
 import { randomUUID } from "crypto";
-import crypto from "crypto";
+import bots from "../../bots.js";
 
-const createHash = (val) => {
-  const hash = crypto.createHash("sha256");
-  hash.update(val);
-  return hash.digest("hex");
-};
+function findAddEvent(event, keyword) {
+  const tBot = bots.find((b) => b.id === keyword.uri);
+  if (tBot && tBot.pk === event.pubkey) {
+    console.log("found add event");
+    return event.id;
+  } else {
+    console.log("did not find add event")
+    return null;
+  }
+}
 
 /**
- * 
- * @param {*} event 
+ *
+ * @param {*} event
  * @returns {Array[Object] | Array[] }
  */
 export const parseEvent = (event) => {
   const documents = event.tags.flatMap((tag) => {
     if (tag[0] === "#m" || tag[0] === "metadata") {
-      const {id, ...metadata} = JSON.parse(tag[1]);
-      
-      if (!id) return null
-      // hash the uri to get the id
+      const { id, ...metadata } = JSON.parse(tag[1]);
+
+      if (!id) return null;
 
       metadata["uri"] = id;
-      metadata["id"] = randomUUID()
+      metadata["id"] = randomUUID();
       metadata["created_at"] = event.created_at;
-      metadata["eventID"] = event.id
-      metadata["eventIDs"] = []
+      metadata["eventID"] = event.id;
+      metadata["eventIDs"] = [];
+      metadata["keywords"] = metadata.keywords.map((keyword) => ({
+        ...keyword,
+        addedByEvent: findAddEvent(event, keyword),
+      }));
+      metadata["resourceType"] = metadata.resourceType.map((keyword) => ({
+        ...keyword,
+        addedByEvent: findAddEvent(event, keyword),
+      }));
       // TODO clean up the metadata of tags and resourceType?
-      return metadata
+      return metadata;
     } else {
-      return null
+      return null;
     }
   });
   return documents.filter((doc) => doc !== null);
--- src/etl/transform.js
+++ src/etl/transform.js
@@ -1,21 +1,62 @@
 // combine the notes with same created_at to one document
 import { randomUUID } from "crypto";
 
+// Check if all objects in an array have defined values for all keys
+const isAllDefined = (arr) => {
+  return arr.every((obj) => {
+    return Object.values(obj).every((val) => {
+      return val !== null && val !== undefined;
+    });
+  });
+};
+
+const merge = (arr1, arr2) => {
+  if (isAllDefined(arr1)) {
+    return arr1;
+  } else if (isAllDefined(arr2)) {
+    return arr2;
+  }
+  const merged = arr1.reduce((acc, obj, index) => {
+    const newObj = Object.assign({}, obj);
+    Object.keys(obj).forEach(key => {
+      if (key === 'addedByEvent' && (newObj[key] === undefined || newObj[key] === null)) {
+        const otherObj = index === 0 ? arr2.find(o => o.id === obj.id && o.addedByEvent !== undefined && o.addedByEvent !== null) : arr1.find(o => o.id === obj.id && o.addedByEvent !== undefined && o.addedByEvent !== null);
+        if (otherObj) {
+          newObj[key] = otherObj[key];
+        }
+      } else if (newObj[key] === undefined || newObj[key] === null) {
+        const otherObj = index === 0 ? arr2.find(o => o[key] !== undefined && o[key] !== null) : arr1.find(o => o[key] !== undefined && o[key] !== null);
+        if (otherObj) {
+          newObj[key] = otherObj[key];
+        }
+      }
+    });
+    acc.push(newObj);
+    return acc;
+  }, []);
+  return merged;
+};
+
+
 export const transform = (documents) => {
   const transformed = documents.reduce((accumulator, currentValue) => {
-    const existingObj = accumulator.find(obj => obj.created_at === currentValue.created_at);
-  
+    const existingObj = accumulator.find(
+      (obj) => obj.created_at === currentValue.created_at
+    );
+
     if (existingObj) {
-      existingObj.id = randomUUID()
+      existingObj.id = randomUUID();
       // name must be the same
       // created_at must be the same
-      // existingObj.keywords.push(currentValue.keywords)
-      // existingObj.resourceType.push(currentValue.resourceType)
-      existingObj.eventIDs.push(currentValue.eventID)
+      const keywords = merge(currentValue.keywords, existingObj.keywords);
+      const resourceTye = merge(existingObj.resourceType, currentValue.resourceType);
+      existingObj.keywords = keywords;
+      existingObj.resourceType = resourceTye;
+      existingObj.eventIDs.push(currentValue.eventID);
     } else {
       accumulator.push(currentValue);
     }
     return accumulator;
   }, []);
   return transformed;
-}
+};
