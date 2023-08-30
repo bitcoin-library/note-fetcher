e2d1a635f112822f4ac9e789c5485c9352db64fa
@s.roertgen <sroertgen@gmail.com>
2023-08-30 18:30:11 +0200

    add dotenv and parse master key from variable

--- src/search/client.js
+++ src/search/client.js
@@ -1,7 +1,12 @@
+import dotenv from "dotenv"
 import { MeiliSearch } from 'meilisearch'
+
+dotenv.config()
+
+const MASTER_KEY = process.env.MASTER_KEY
 
 export const client = new MeiliSearch({
   host: 'http://search:7700',
-  apiKey: 'MASTER_KEY',
+  apiKey: MASTER_KEY,
 })
 
